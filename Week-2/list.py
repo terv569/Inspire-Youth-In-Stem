3d224b0241ed0acacd02745c907b394ca17b316b
TERV <tervilmoywaywa@gmail.com>
2023-02-14 10:43:58 +0300

    lists in python

---
+++ Week-2/list.py
@@ -0,0 +1,5 @@
+names=["Tervil,""Mary,""John,""Keith,""Sam,"]
+
+
+
+print(names[:3])
---
+++ Week-2/list2.py
@@ -0,0 +1,35 @@
+fruits=["Apple","mango","pear","orange","pawpaw","banana","guava",]
+print(fruits[0:-1])
+print(fruits[0:3])
+print([3])
+print("my favourite fruit is",fruits[0].upper())
+#Adding two lists
+vegetables = ["kales","spinach","cabbage","broccoli","cauliflower"]
+stationery = ["pens","paper","scissors","stapler","ink"]
+shopping = vegetables+stationery
+print(shopping)
+print(shopping[5])
+for vegetable in vegetables:
+    print(vegetable)
+for shoppi in shopping:
+    print(shopping)
+
+
+
+names = ("terv","john","ken")
+
+
+
+
+
+
+
+
+
+
+
+
+print("my name is "+ names[0] + " and my favourite fruit is " + fruits[0])
+
+
+
